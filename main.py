dddb710e7d82a80d3d4c4a551d7deb066cb84a8a
zhung1oz7 <wl00494849@gmail.com>
2023-10-24 22:07:12 +0800

    update

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import requests
-import pandas
+import pandas as pd
 from datetime import datetime
 import os
 
-def GetOpenData ():
+time = datetime.now()
+fileName = f"DataSet/OpenDataSet_{time.year}_{time.month}_{time.day}.csv"
+
+def FileCheck(name:str):
+    if os.path.isfile(name):
+        os.remove(name)
+
+def GetOpenData (): 
     domain_Url = 'https://data.gov.tw/api/front/dataset/export?format=csv'
     rep = requests.get(domain_Url)
-    time = datetime.now()
-    fileName = f"{time.year}_{time.month}_{time.day}.csv"
-    if os.path.isfile(f"DataSet/{fileName}"):
-        os.remove(f"DataSet/{fileName}")
-    open(f"/Users/zhung/py/meeting/DataSet/{fileName}","wb").write(rep.content)
+    FileCheck(fileName)
+    open(f"/Users/zhung/py/meeting/{fileName}","wb").write(rep.content)
+
+def NewCSV():
+    dt = pd.read_csv(fileName)
+    df = pd.DataFrame(dt)
+    d = df[["資料集名稱","資料下載網址"]]
+    FileCheck("new.csv")
+    d.to_csv("new.csv")
 
 def AnalyzeCSV():
-    time = datetime.now()
-    fileName = f"{time.year}_{time.month}_{time.day}.csv"
-    df = pandas.read_csv(f"DataSet/{fileName}") 
-    print(df.columns)
-    print(df["資料集名稱"].value_counts)
-       
+    print("Input your key word")
+    keyWord = input()
+    print(f"search keyword : {keyWord}....")
+    dt = pd.read_csv("new.csv")
+    q = dt[dt.資料集名稱.str.contains(keyWord)]
+    print(q.values)
 
 def Main():
     GetOpenData()
+    NewCSV()
     AnalyzeCSV()
 
 
