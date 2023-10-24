fc833589f40a2363f54cf4009d367fbd05c52c2f
zhung1oz7 <wl00494849@gmail.com>
2023-10-24 11:03:01 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+import pandas
+from datetime import datetime
+
+def GetOpenData ():
+    domain_Url = 'https://data.gov.tw/api/front/dataset/export?format=csv'
+    rep = requests.get(domain_Url)
+    time = datetime.now()
+    open(f"/Users/zhung/py/meeting/DataSet/{time.year}_{time.month}_{time.day}","wb").write(rep.content)
+    
+def main():
+    GetOpenData()
+
+main()
